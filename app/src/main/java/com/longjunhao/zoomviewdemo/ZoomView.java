483a9f1c9b61b46c77aa6ce4b3bad3a7f8fb7db2
longjunhao <longjunhao@eben.cn>
2020-03-05 17:13:39 +0800

    view 实现单指移动，双指旋转和缩放功能

---
+++ app/src/main/java/com/longjunhao/zoomviewdemo/ZoomView.java
@@ -0,0 +1,125 @@
+package com.longjunhao.zoomviewdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+
+/**
+ * 参考博客:https://www.jianshu.com/p/df34e47bfa2a
+ * ZoomView 是一个可以实现单指平移，双指缩放和旋转的view，内部包含的view也会跟着一起变化，示例继承自
+ * RelativeLayout，也可以根据自己需要继承自所有View类的子类
+ */
+
+public class ZoomView extends RelativeLayout {
+
+    private static final String TAG = "ZoomView";
+    // 属性变量
+    private float translationX; // 移动X
+    private float translationY; // 移动Y
+    private float scale = 1; // 伸缩比例
+    private float rotation; // 旋转角度
+
+    // 移动过程中临时变量
+    private float actionX;
+    private float actionY;
+    private float spacing;
+    private float degree;
+    private int moveType; // 0=未选择，1=拖动，2=缩放
+
+    public ZoomView(Context context) {
+        this(context, null);
+    }
+
+    public ZoomView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ZoomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setClickable(true);
+    }
+
+    /**
+     * toolType=1：手指触摸屏幕
+     * toolType=2：手写笔触摸屏幕
+     * toolType = 4: 橡皮擦
+     * 如果想实现，当手指触控缩放，手写笔不能控制时，就需要用下面方法中注释掉的那部分了。
+     *
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.d(TAG, "onInterceptTouchEvent: ljh");
+        getParent().requestDisallowInterceptTouchEvent(true);
+        return super.onInterceptTouchEvent(ev);
+//        int toolType = ev.getToolType(0);
+//        if (toolType == 1) {
+//            return true;
+//        } else {
+//            return super.onInterceptTouchEvent(ev);
+//        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: ljh ");
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                moveType = 1;
+                actionX = event.getRawX();
+                actionY = event.getRawY();
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                moveType = 2;
+                spacing = getSpacing(event);
+                degree = getDegree(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (moveType == 1) {
+                    translationX = translationX + event.getRawX() - actionX;
+                    translationY = translationY + event.getRawY() - actionY;
+                    setTranslationX(translationX);
+                    setTranslationY(translationY);
+                    actionX = event.getRawX();
+                    actionY = event.getRawY();
+                } else if (moveType == 2) {
+                    scale = scale * getSpacing(event) / spacing;
+                    setScaleX(scale);
+                    setScaleY(scale);
+                    rotation = rotation + getDegree(event) - degree;
+                    if (rotation > 360) {
+                        rotation = rotation - 360;
+                    }
+                    if (rotation < -360) {
+                        rotation = rotation + 360;
+                    }
+                    setRotation(rotation);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                moveType = 0;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    // 触碰两点间距离
+    private float getSpacing(MotionEvent event) {
+        //通过三角函数得到两点间的距离
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    // 取旋转角度
+    private float getDegree(MotionEvent event) {
+        //得到两个手指间的旋转角度
+        double delta_x = event.getX(0) - event.getX(1);
+        double delta_y = event.getY(0) - event.getY(1);
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
+    }
+}
